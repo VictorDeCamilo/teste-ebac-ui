4a44b48c6c324973f125ff9557fdabf5be205d37
Victor De Camilo <victor.camilo@finnet.com.br>
2022-06-01 12:20:44 -0300

    Page objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+import EnderecoPage from '../support/page-objects/endereco.page';
 const perfil = require('../fixtures/perfil.json')
 
 describe('Funcionalidade: Endereços - faturamento e entrega', () => {
@@ -9,7 +10,8 @@ describe('Funcionalidade: Endereços - faturamento e entrega', () => {
         cy.login(perfil.usuario, perfil.senha)
     });
 
-    it('Cadastro de faturamento com sucesso', () => {
-        
+    it.only('Cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Flávio', 'Araújo', 'Google', 'Avenida Brasil', '311', 'São Paulo', 'São Paulo', '01222000', '11982298272', 'flavio@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
     });
 });
